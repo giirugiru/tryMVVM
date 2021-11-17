84d1f7ce3896309e7074818f0ed1f7cf20bf8414
Gilang Sinawang <giirugiru@gmail.com>
2021-11-17 14:40:07 +0700

    Initial Commit

---
+++ tryMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tryMVVM
+//
+//  Created by Gilang Sinawang on 17/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
